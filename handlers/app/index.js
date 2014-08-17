0955f5684e8878451ccf603247692f0c7ff745f5
Eirik Vullum <evullum@gmail.com>
2014-08-17 20:15:46 +0200

    add missing separator

--- handlers/app/index.js
+++ handlers/app/index.js
@@ -1,7 +1,7 @@
 exports = module.exports = function (services, helpers) {
     return {
         home: require('./home')(services, helpers),
-        article: require('./article')(services, helpers)
+        article: require('./article')(services, helpers),
         keepalive: require('./keepalive')(services, helpers)
     };
 };
