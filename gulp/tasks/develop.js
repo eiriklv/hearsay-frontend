e2bd90c9ef6ea73d157b643c02c06c8f7e8f916f
Eirik Langholm Vullum <eirik.vullum@sol.no>
2014-09-26 10:14:55 +0200

    use legacy watch mode

--- gulp/tasks/develop.js
+++ gulp/tasks/develop.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('develop', function (){
-    nodemon({ script: 'app.js', ext: 'html js handlebars styl', ignore: ['client/public/**', 'node_modules/**'] })
-    .on('restart', ['build']);
+gulp.task('develop', function() {
+    nodemon({
+        script: 'app.js',
+        ext: 'html js handlebars styl',
+        ignore: ['client/public/**', 'node_modules/**'],
+        legacyWatch: true
+    })
+        .on('restart', ['build']);
 });
