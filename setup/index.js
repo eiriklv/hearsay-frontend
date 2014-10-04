bd6bdfee649ce2e2fc18a5c2532efdb0d600e272
Eirik Vullum <evullum@gmail.com>
2014-10-04 10:24:03 +0200

    add mongoose error handling and automatic reconnect

--- setup/index.js
+++ setup/index.js
@@ -101,8 +101,49 @@ module.exports.handleExpressError = function (app, helpers) {
 };
 
 // connect to backend store (db)
-module.exports.db = function (db, config)  {
-    db.connect(config.get('database.mongo.url'));
+module.exports.db = function (mongoose, config){
+    function connect() {
+        mongoose.connect(config.get('database.mongo.url'));
+    }
+
+    // connection is open and ready
+    mongoose.connection.on('open', function (ref) {
+        debug('open connection to mongo server.');
+    });
+
+    // mongoose is connected to server
+    mongoose.connection.on('connected', function (ref) {
+        debug('connected to mongo server.');
+    });
+
+    // mongoose has disconnected
+    mongoose.connection.on('disconnected', function (ref) {
+        debug('disconnected from mongo server.');
+
+        debug('retrying connection in 2 seconds..');
+        setTimeout(function() {
+            connect();
+        }.bind(this), 2000);
+    });
+
+    // mongoose connection has closed
+    mongoose.connection.on('close', function (ref) {
+        debug('closed connection to mongo server');
+    });
+
+    // error has occured for mongoose connection
+    mongoose.connection.on('error', function (err) {
+        debug('error connection to mongo server!');
+        debug(err);
+    });
+
+    // mongoose is reconnecting
+    mongoose.connection.on('reconnect', function (ref) {
+        debug('reconnect to mongo server.');
+    });
+
+    // initial connect
+    connect();
 };
 
 // bind server to port
