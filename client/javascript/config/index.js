12674e003acf919e8a27b25f1d3106e529ec75ca
Eirik Vullum <evullum@gmail.com>
2014-10-05 16:00:10 +0200

    log api url

--- client/javascript/config/index.js
+++ client/javascript/config/index.js
@@ -1,6 +1,8 @@
 var apiUrl = process.env.CLOUD ?
     'http://' + process.env.CLIENT_DOMAIN + process.env.CLIENT_API_PATH :
-    'http://' + process.env.CLIENT_DOMAIN + ':' + process.env.PORT +  process.env.CLIENT_API_PATH;
+    'http://' + process.env.CLIENT_DOMAIN + ':' + process.env.PORT + process.env.CLIENT_API_PATH;
+
+console.log(apiUrl);
 
 exports = module.exports = {
     environment: process.env.NODE_ENV,
