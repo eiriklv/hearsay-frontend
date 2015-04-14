e4980c4ce294773d4a1e0db43aae878c112ef51a
Eirik Vullum <evullum@gmail.com>
2015-04-14 13:28:37 +0200

    add route for full article content

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,4 @@
 exports = module.exports = function (app, express, middleware, handlers, path) {
     app.use(path, require('./entries')(express, middleware, handlers, '/entries'));
+    app.use(path, require('./entries-content')(express, middleware, handlers, '/entries-content'));
 };
--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
 exports = module.exports = function (models, helpers) {
     return {
-        entries: require('./entries')(models.Entry, helpers)
+        entries: require('./entries')(models.Entry, helpers),
+        entriesContent: require('./entries')(models.Entry, helpers)
     };
 };
