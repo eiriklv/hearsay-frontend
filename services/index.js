957aad98006fbfc2df6206099a5e3daeb5f1c744
Eirik Vullum <evullum@gmail.com>
2015-04-14 13:43:31 +0200

    require correct file

--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
 exports = module.exports = function (models, helpers) {
     return {
         entries: require('./entries')(models.Entry, helpers),
-        entriesContent: require('./entries')(models.Entry, helpers)
+        entriesContent: require('./entries-content')(models.Entry, helpers)
     };
 };
